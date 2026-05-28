200a6b85f22ac613d4e9ad6b98a2baad6b410c2b
Kritika Kapoor <kritikakapoor131@gmail.com>
2026-05-28 17:46:16 +0530

    Path and Query Parameters

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
 
 import json
 
-app = FastAPI()
+app = FastAPI()         #Initialize 
 
 def load_data():
     with open('patients.json', 'r') as f:
         data = json.load(f)
     return data 
 
-@app.get("/")
-def hello():
-    return {'message':'Patient Management System'}
+@app.get("/")                   #Route, Endpoint
+def hello():                                            
+    return {'message':'Patient Management System'}          #Response
 
 @app.get("/about")
 def about():
@@ -19,5 +19,30 @@ def about():
 
 @app.get("/view")
 def view():
+    data = load_data()          #Load data from Json file
+    return data                 #return data as response
+
+
+@app.get("/patient/{patient_id}")               #to find the info of a specific patient  
+def view_patient(patient_id:str = Path(...,description='ID of patient in the DB', example='P001')):
     data = load_data()
-    return data
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail='Patient not found')
+
+@app.get("/sort")
+def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'), order: str = Query('asc', description='Sort in asc or desc')):
+    valid_feilds = ['height', 'weight', 'bmi']
+    if sort_by not in valid_feilds:
+        raise HTTPException(status_code=400, detail = f'Invalid feild select from {valid_feilds}')
+    
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400, detail='Invalid order select by asc or desc')
+    
+    data = load_data()  
+
+    sort_order = True if order=='desc' else False
+
+    sorted_data = sorted(data.values(), key=lambda x:x.get(sort_by, 0), reverse=sort_order)
+
+    return sorted_data
