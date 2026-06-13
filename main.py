eef70c9b71bfd6aa8030770cd060cab26324633e
Kritika Kapoor <kritikakapoor131@gmail.com>
2026-06-13 23:17:58 +0530

    update CORS for Vercel

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://meditrack-two-sand.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
